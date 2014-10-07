5ed0d74da2d99e59ce2dec13a87559d82d5a0909
centip3de <goodkingkyle@gmail.com>
2014-10-06 19:08:34 -0700

    Added set opcode and removed the previous classes.

--- src/emulator.py
+++ src/emulator.py
@@ -1,37 +1,93 @@
 import sys
 from opcodes import *
 
-class Memory():
-
-    mem = [0]*0x10000
-
-    def get_mem(addr):
-        return Memory.mem[addr]
-
-    def set_mem(addr, foo):
-        Memory.mem[addr] = foo
-
 class CPU():
+
     def __init__(self, text):
-        self.text = text.splitlines()
+        self.text = text.split()
+        self.mem = [0]*0x10000
+        self.regs = [0] * 8
+        self.PC = 0
+        self.O = 0
+        self.SP = 0
 
     def run(self):
-        for line in self.text:
-            self.step(line.split())
+        self.load(self.text)
 
-    def decode(self, line):
-        word = int(line[0], 0)
+        while(self.PC != len(self.text)):
+            self.step()
+
+        self.reg_dump()
+
+    def reg_dump(self):
+        print("********* REG DUMP *********")
+        for i in range(len(self.regs)):
+            print(REV_VALUES[i] + ": " + str(hex(self.regs[i])))
+
+        print("PC: " + str(hex(self.PC)))
+
+    def cycle(self, amount):
+        while amount > 0:
+            amount -= 1
+
+    def decode(self, word):
         op = word & 0xF
-        dest = word & 0x3F0
+        dest = (word & 0x3F0) >> 4
         src = word >> 10
-
-        if (len(line) >= 2):
-            return [op, dest, src, line[1:]]
 
         return [op, dest, src]
 
-    def step(self, line):
-        print(self.decode(line))
+    def SET(self, dest, src):
+        if self.is_reg(dest):
+            if dest == 0x1C:
+                self.PC = src
+            else:
+                self.regs[dest] = src
+
+        else:
+            self.mem[dest] = src
+
+        self.cycle(1)
+
+    def step(self):
+        word = self.decode(self.get_next())
+        op = word[0]
+        dest = word[1]
+        src  = word[2]
+
+        
+        if op in REV_BASIC:
+
+            if src >= 0x20 and src <= 0x3f:
+                src -= 32
+
+            if dest >= 0x20 and dest <= 0x3f:
+                dest -= 32
+
+            if dest == 0x1e:
+                dest = self.get_next()
+
+            if src == 0x1f:
+                src = self.get_next()
+
+            if src == 0x1e:
+                src = self.get_next()
+
+            
+            if REV_BASIC[op] == "SET":
+                self.SET(dest, src)
+
+    def load(self, program):
+        for i in range(len(program)):
+            self.mem[i] = int(program[i], 0)
+
+    def get_next(self):
+        mem = self.mem[self.PC]
+        self.PC += 1
+        return mem
+
+    def is_reg(self, reg):
+        return (reg in REV_VALUES) and ((reg >= 0x0 and reg <= 0x07) or (reg >= 0x1B and reg <= 0x1D))
 
 def main():
     if len(sys.argv) != 2:
