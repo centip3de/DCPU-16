daecb2e1889df9759dafcbce6f9ed38ba963a78d
centip3de <goodkingkyle@gmail.com>
2014-11-10 16:38:31 -0800

    Created a script to handle both the assembling and emulating process, as well as running the gui on said files.

---
+++ src/main.py
@@ -0,0 +1,32 @@
+import sys
+import emulator 
+import assembler
+import gui
+import argparse
+
+def main():
+
+    parser = argparse.ArgumentParser(description = "Automate the assembling and emulation of the given file.")
+    parser.add_argument("filepath", help="The path to the .dasm file to run.")
+    parser.add_argument("-g", action="store_true", help="Enable the GUI") 
+
+    args = parser.parse_args()
+    
+    filepath = args.filepath
+    guiFlag = args.g
+
+    fi = open(filepath, "r")
+    text = fi.read()
+    fi.close()
+
+    p = assembler.Parser(text).assemble()
+    cpu = emulator.CPU(p)
+
+
+    if not guiFlag:
+        cpu.run()
+    else:
+        gui.main(p)
+
+if __name__ == "__main__":
+    main()
