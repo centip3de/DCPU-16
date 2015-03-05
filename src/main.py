cef755bda8a2c0a3db7fd29a7567bd099058baf7
centip3de <goodkingkyle@gmail.com>
2015-03-05 14:48:26 -0800

    Added a flag to mark whether the code you're running is already assembled or not.

--- src/main.py
+++ src/main.py
@@ -19,24 +19,32 @@ def main():
     parser = argparse.ArgumentParser(description = "Automate the assembling and emulation of the given file.")
     parser.add_argument("filepath", help="The path to the .dasm file to run.")
     parser.add_argument("-g", action="store_true", help="Enable the GUI") 
+    parser.add_argument("-a", action="store_true", help="Mark that the code is already assembled")
 
     args = parser.parse_args()
     
     filepath = args.filepath
     guiFlag = args.g
+    assembledFlag = args.a
 
     fi = open(filepath, "r")
     text = fi.read()
     fi.close()
 
-    p = assembler.Parser(text).assemble()
-    cpu = emulator.CPU(p)
+    if(assembledFlag):
+        cpu = emulator.CPU(text)
+    else:
+        p = assembler.Parser(text).assemble()
+        cpu = emulator.CPU(p)
 
 
     if not guiFlag:
         cpu.run()
     else:
-        gui.main(p)
+        if(assembledFlag):
+            gui.main(text)
+        else:
+            gui.main(p)
 
 if __name__ == "__main__":
     main()
