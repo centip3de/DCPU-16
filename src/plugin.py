552a1994eb0d510b8f9a5142a94905130afeb1b2
centip3de <goodkingkyle@gmail.com>
2015-03-05 14:49:28 -0800

    Added a RegisterListener that listens for changes in the registers, and PrintListener that listens for print statements

--- src/plugin.py
+++ src/plugin.py
@@ -4,6 +4,8 @@ class PluginManager():
     #List of all listeners
     listeners = []
     mem_list  = []
+    reg_list  = []
+    pnt_list  = []
 
     def add(listener, list_type):
         #Add a new listener to the list
@@ -14,13 +16,39 @@ class PluginManager():
             PluginManager.mem_list.append(listener)
             print("[PLUGIN] Added new Memory Listener:", listener)
 
+        if(list_type == 2):
+            PluginManager.reg_list.append(listener)
+            print("[PLUGIN] Added new Register Listener:", listener)
+
+        if(list_type == 3):
+            PluginManager.pnt_list.append(listener)
+            print("[PLUGIN] Added new Print Listener:", listener)
+
     def notify(event, data):
         if(event == 1):
             for listener in PluginManager.mem_list:
                 listener.notify(data)
 
+        if(event == 2):
+            for listener in PluginManager.reg_list:
+                listener.notify(data)
+
+        if(event == 3):
+            for listener in PluginManager.pnt_list:
+                listener.notify(data)
+
     def unregister(listener):
         PluginManager.listeners.remove(listener)
+
+        if(listener in PluginManager.mem_list):
+            PluginManager.mem_list.remove(listener)
+
+        elif(listener in PluginManager.reg_list):
+            PluginManager.reg_list.remove(listener)
+
+        elif(listener in PluginManager.pnt_list):
+            PluginManager.pnt_list.remove(listener)
+
         print("[PLUGIN] Removed listner:", listener)
             
 class MemoryListener():
@@ -36,3 +64,28 @@ class MemoryListener():
 
     def unregister(self):
         PluginManager.unregister(self)
+
+class RegisterListener():
+    def __init__(self, action):
+        PluginManager.add(self, 2)
+
+        self.action = action
+
+    def notify(self, data):
+        self.action(data)
+
+    def unregister(self):
+        PluginManager.unregister(self)
+
+class PrintListener():
+    def __init__(self, action):
+        PluginManager.add(self, 3)
+
+        self.action = action
+
+    def notify(self, data):
+        self.action(data)
+
+    def unregister(self):
+        PluginManager.unregister(self)
+
